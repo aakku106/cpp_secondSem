5ffaa5f86debbf2867d18f7cea624914a4940309
Adarasha Gaihre <adarashagaihre80@nepathyacollege.edu.np>
2024-08-30 13:23:24 +0545

    Refactor code to improve Binary class implementation

--- ytLearn/3nesting.cpp
+++ ytLearn/3nesting.cpp
@@ -4,10 +4,10 @@ using namespace std;
 class Binary
 {
     string s;
+    void chk_bin(void);
 
 public:
     void read(void);
-    void chk_bin(void);
     void ones(void);
     void display(void);
 };
@@ -33,36 +33,36 @@ void Binary ::chk_bin(void)
 void Binary ::ones(void)
 {
     chk_bin();
-    cout<<"\n";
+    cout << "\n";
     for (int i = 0; i < s.length(); i++)
     {
         if (s.at(i) == '0')
         {
-            s.at(i) = '1';
+            s.at(i)= '1';
         }
         else
         {
             s.at(i) = '0';
         }
     }
-    cout << "The ones complement of the binary number is " << s << endl;
+    cout << "The ones complement of the binary number is " << s << endl; // 😙
 }
 
 void Binary ::display(void)
 {
-    cout << "The binary number is\t";
-    for (int i = 0; i < s.length(); i++)
-    {
-        cout
-            << s.at(i);
-    }
+    cout << "The binary number is\t " << s << endl; // we can also direct emplement like we used in ones() in the above function 😙
+    // for (int i = 0; i < s.length(); i++)  // This is also correct
+    // {                                    // But the above one is more efficient
+    //     cout                             // as it doesn't need to loop through the string
+    //         << s.at(i);                                 // to get the length of the string
+    // }                                            // every time
 }
 
 int main()
 {
     Binary b;
     b.read();
-    b.chk_bin();
+    // b.chk_bin();
     b.display();
     b.ones();
     return 0;
