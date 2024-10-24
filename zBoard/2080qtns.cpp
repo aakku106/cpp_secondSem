d715e107d88cde6902ed91f486d7f36f8f53bf88
aakku106 <adarashagaihre80@nepathyacollege.edu.np>
2024-10-24 23:57:59 +0545

    Refactor Height class constructor and operator+ overload Simpliying it

--- zBoard/2080qtns.cpp
+++ zBoard/2080qtns.cpp
@@ -8,10 +8,14 @@ class Height
 
 public:
     // Constructor to initialize meter and centimeter
-    Height(int m = 0, int cm = 0) : meter(m), centimeter(cm) {}
+    Height(int m = 0, int cm = 0)
+    {
+        meter = m;
+        centimeter = cm;
+    }
 
     // Friend function to overload the + operator
-    friend Height operator+(const Height &h1, const Height &h2);
+    friend Height operator+(Height, Height);
 
     // Function to display height
     void display() const
@@ -21,10 +25,10 @@ public:
 };
 
 // Overloading the + operator using a friend function
-Height operator+(const Height &h1, const Height &h2)
+Height operator+(Height h1, Height h2)
 {
     Height r;
-    r.centimeter = h1.centimeter + h2.centimeter;          // aadding both centimeters  
+    r.centimeter = h1.centimeter + h2.centimeter;         // aadding both centimeters
     r.meter = h1.meter + h2.meter + (r.centimeter / 100); // Convert cm to meters
     r.centimeter = r.centimeter % 100;                    // Remaining centimeters
     return r;
@@ -33,9 +37,8 @@ Height operator+(const Height &h1, const Height &h2)
 int main()
 {
     Height h1(2, 90), // 2 meters, 90 centimeters
-        h2(1, 30);    // 1 meter, 30 centimeters
-
-    Height h3 = h1 + h2; // Add the two heights
+        h2(1, 30),    // 1 meter, 30 centimeters
+        h3 = h1 + h2; // Add the two heights
 
     cout << "Height 1: ";
     h1.display();
